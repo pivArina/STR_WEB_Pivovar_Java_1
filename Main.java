bd8d86600f1ffd39468db0943c2b76b9e976aa6d
pivArina <144273716+pivArina@users.noreply.github.com>
2024-09-12 14:56:47 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Arina");
+        System.out.println("My group is 273601");
+        // sdhfkshfkjshfkshfkshfkshfkshfkjshfkshf
+        System.out.println("isit");
+
+    }
+}
