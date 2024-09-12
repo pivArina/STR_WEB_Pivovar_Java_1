c5cb5ff01a5cc5342a3d115b255a5f477fcdcc1b
pivArina <144273716+pivArina@users.noreply.github.com>
2024-09-12 14:59:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, I am Arina");
         System.out.println("My group is 273601");
-        // sdhfkshfkjshfkshfkshfkshfkshfkjshfkshf
         System.out.println("isit");
-
+        System.out.println("Иришка, я изменила");
     }
 }
